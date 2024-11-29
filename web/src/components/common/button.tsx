552add6b589c3ab78265b31bdecf5e9f5c284685
Kacper Hemperek <kacper.hemperek@apptimia.com>
2024-11-29 18:13:41 +0100

    feat: add button element

---
+++ web/src/components/common/button.tsx
@@ -0,0 +1,33 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "../../utils/cn";
+
+const button = cva("px-3 py-1 flex items-center justify-center rounded-full", {
+  variants: {
+    variant: {
+      primary: ["bg-blue-500"],
+      success: ["bg-green-500"],
+      danger: ["bg-red-500"],
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+type ButtonProps = React.PropsWithChildren<
+  {} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+    VariantProps<typeof button>
+>;
+
+export const TwButton = ({
+  children,
+  variant,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button className={cn(button({ variant, className }))} {...props}>
+      {children}
+    </button>
+  );
+};
